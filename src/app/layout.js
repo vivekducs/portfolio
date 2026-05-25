f4589e1142ca6225bef510f6e954fd3afdb33a3e
AVPXM8 <113023480+AVPXM8@users.noreply.github.com>
2026-05-25 11:41:40 +0530

    updated gsc and analytics tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
@@ -57,6 +58,9 @@ export const metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "eWcF9dyaiktE_gTkn88SxvgDKUQIu5YDCgncdNA1oqk",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -87,6 +91,24 @@ export default function RootLayout({ children }) {
       </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1B1PNL16LK"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1B1PNL16LK');
+            `,
+          }}
+        />
       </body>
     </html>
   );
